3d4e4a9e03e25b3c28b8ddc4fc9b1c362e9a9fb5
Jeremy Spell <jspell@gmail.com>
2020-02-04 05:29:28 -0600

    Again

--- services/websrv-dock/web-local.tf
+++ services/websrv-dock/web-local.tf
@@ -14,17 +14,17 @@ resource "docker_container" "websrv-dock" {
 
 variable "local_dock_image" {
     description = "Local Docker image to start up for test."
-    type = string
+    
 }
 
 variable "local_dock_port_int" {
     description = "Internal docker port to use."
-    type = number
+    
 }
 
 variable "local_dock_port_ext" {
     description = "External docker port to use."
-    type = number
+    
 }
 
 variable "local_dock_host" {
