46477f3be3a4a9bc62145df7b7eaa8a288e6207d
Jeremy Spell <jspell@gmail.com>
2020-02-05 12:53:36 -0600

    Updated variable for zone id

--- mgmt/dns/aws-dns.tf
+++ mgmt/dns/aws-dns.tf
@@ -4,15 +4,15 @@ provider "aws" {
 }
 
 resource "aws_route53_record" "hostname" {
-  zone_id = var.aws_region
+  zone_id = var.aws_zoneid
   name    = var.hostname
   type    = "A"
   ttl     = "300"
   records = [var.public_ip]
 }
 
-variable "aws_region" {
-    description = "AWS region for Route 53"
+variable "aws_zoneid" {
+    description = "Zone ID for update on Route 53"
 }
 
 variable "hostname" {
