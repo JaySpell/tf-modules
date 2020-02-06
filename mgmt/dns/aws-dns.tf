7b52b08c0fd5fe4295229c76a3dfee25b12105c1
Jeremy Spell <jspell@gmail.com>
2020-02-05 21:49:05 -0600

    Got it this time

--- mgmt/dns/aws-dns.tf
+++ mgmt/dns/aws-dns.tf
@@ -1,12 +1,12 @@
 resource "aws_route53_record" "hostname" {
-  zone_id = var.aws_zoneid
+  zone_id = var.zoneid
   name    = var.hostname
   type    = "A"
   ttl     = "300"
   records = [var.public_ip]
 }
 
-variable "aws_zoneid" {
+variable "zoneid" {
     description = "Zone ID for update on Route 53"
 }
 
