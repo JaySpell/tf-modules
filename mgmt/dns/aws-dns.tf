a9c0d1161c5072c746bbcbc18e89e90fc1c69b76
Jeremy Spell <jspell@gmail.com>
2020-02-06 09:21:56 -0600

    Modifying for zone id..

--- mgmt/dns/aws-dns.tf
+++ mgmt/dns/aws-dns.tf
@@ -1,12 +1,16 @@
+resource "aws_route53_zone" "dnszone" {
+    name = var.dnszone
+}
+
 resource "aws_route53_record" "default" {
-  zone_id = var.zoneid
+  zone_id = aws_route53_zone.dnszone.zone_id
   name    = var.hostname
   type    = "A"
   ttl     = "300"
   records = [var.public_ip]
 }
 
-variable "zoneid" {
+variable "dnszone" {
     description = "Zone ID for update on Route 53"
 }
 
