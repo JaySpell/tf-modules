94426c44ee079d0c4b2c8c6bd84d1fb6b3bd735f
Jeremy Spell <jspell@gmail.com>
2020-02-08 11:22:44 -0600

    Added ingress port 22

--- security/aws-secgroup.tf
+++ security/aws-secgroup.tf
@@ -1,8 +1,13 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
+resource "aws_security_group" "allow_srv" {
+  name        = "allow_srv"
   description = "Allow HTTP inbound traffic"
   vpc_id      = var.vpcid
-
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = var.from_port
     to_port     = var.to_port
