4f48fc65a17358d4856f57b8cca4243975722746
Jeremy Spell <jspell@gmail.com>
2020-02-07 21:51:04 -0600

    Added AWS security group

---
+++ security/aws-secgroup.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = # add a CIDR block here
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+variable "aws_vpc" {
+    description = "AWS VPC"
+}
+
+variable "from_port" {
+    description = "Ingress from port"
+}
+
+variable "to_port" {
+    description = "Ingress to port"
+}
