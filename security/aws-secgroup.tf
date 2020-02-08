7f5a509af5f584214119e2e99ff6d7ff18b40997
Jeremy Spell <jspell@gmail.com>
2020-02-08 08:17:12 -0600

    Removed prefix list id

--- security/aws-secgroup.tf
+++ security/aws-secgroup.tf
@@ -18,7 +18,6 @@ resource "aws_security_group" "allow_http" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 }
 
